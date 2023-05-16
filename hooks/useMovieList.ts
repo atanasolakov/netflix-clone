73160e4707cb94fde58df3e30977b83a56b7fb17
Your Name <you@example.com>
2023-05-16 13:10:13 +0200

    Initial commit

---
+++ hooks/useMovieList.ts
@@ -0,0 +1,17 @@
+import fetcher from '@/lib/fetcher';
+import useSWR from "swr";
+
+const useMovies = () => {
+    const { data, error, isLoading } = useSWR('/api/movies', fetcher, {
+        revalidateIfStale: false,
+        revalidateOnFocus: false,
+        revalidateOnReconnect: false,
+    });
+    return {
+        data,
+        error,
+        isLoading
+    }
+};
+
+export default useMovies;
